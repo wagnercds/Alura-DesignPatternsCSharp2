ed2ae8ced17ee58f5155375b578725dc5d07ab56
Wagner <wagnercarmo@hotmail.com>
2020-07-04 17:24:00 -0300

    Respostas do capitulo Facade e Singleton

---
+++ Alura-DesignPatternsCSharp2/FacadeSingleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace FacadeSingleton
+{
+    class Servico
+    {
+
+    }
+
+    class FactoryServico
+    {
+        private Servico servico = new Servico();
+
+        public Servico PegaInstancia => servico;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
