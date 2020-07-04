3b36b13c9967ad59bd2fb8f64374966571d7aa4f
Wagner <wagnercarmo@hotmail.com>
2020-07-04 17:39:17 -0300

    Respostas do capitulo fabrica e o problema de criação de objetos

---
+++ Factory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Factory
+{
+    static class ConnectionFactory
+    {
+        public static IDbConnection GetConnection(string strConnection)
+        {
+            var connection = new SqlConnection(strConnection);
+            connection.Open();
+            return connection;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factory Get SQL Connection");
+            IDbConnection connection = ConnectionFactory.GetConnection("connection string");
+        }
+    }
+}
