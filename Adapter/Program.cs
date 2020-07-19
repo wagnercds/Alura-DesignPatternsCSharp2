4a36586a7c3f7d48e6835995a3f3672fbd3c3661
Wagner <wagnercarmo@hotmail.com>
2020-07-19 15:07:09 -0300

    Respostas do capitulo Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace Adapter
+{
+    public class GeradorXML<T>
+    {
+        public string XML(T objeto)
+        {
+            var serializador = new XmlSerializer(objeto.GetType());
+            var stringWriter = new StringWriter();
+            serializador.Serialize(stringWriter, objeto);
+            var result = stringWriter.ToString();
+            return result;
+        }
+    }
+
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+
+        public Cliente()
+        {
+
+        }
+
+        public Cliente(string nome, string endereco, string telefone)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Telefone = telefone;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cliente = new Cliente("teste nome", "teste endereco", "11111-11111");
+            var geradorXML = new GeradorXML<Cliente>();
+            string xml = geradorXML.XML(cliente);
+            Console.WriteLine("XML: {0}", xml);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Alura Design Patterns C# Módulo 2
+
+Respostas dos exercícios Alura Design Patterns C# Módulo 2, conforme o meu entendimento e conhecimento
+
+Todas as classe são criadas no mesmo arquivo já que é a forma de enviar as repostas para a Alura
